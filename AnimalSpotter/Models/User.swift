2fac92b934ec82a47def11e99ca511ed68e68b56
Michael Stoffer <mstoffer@michaelstoffer.com>
2019-05-29 21:50:23 -0400

    Finished setting up models for project

---
+++ AnimalSpotter/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  AnimalSpotter
+//
+//  Created by Michael Stoffer on 5/29/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let username: String
+    let password: String
+}
